6586216a361bd9fa9307373e3e9e899aece11de9
k-vernon <kvernon908@gmail.com>
2022-12-19 12:44:45 -0500

    add to DOM elements

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 
 
 /*-------------------------------- Constants --------------------------------*/
-// import characters from "./data.js"
+const characters = [
+    { number: 1, name: "Amour", gender: "male", hair: "black", eyes:"green", glasses: false, facialHair: false, bald: false, jewelry: true},
+    { number: 2, name: "Ashley", gender: "female", hair: "black", eyes:"blue", glasses: false, facialHair: false, bald: false, jewelry: true},
+    { number: 3, name: "Bella", gender: "female", hair: "brown", eyes:"brown", glasses: false, facialHair: false, bald: false, jewelry: false},
+    { number: 4, name: "Ember", gender: "female", hair: "red", eyes:"green", glasses: false, facialHair: false, bald: false, jewelry: true},
+    { number: 5, name: "Khalil", gender: "male", hair: "black", eyes:"brown", glasses: false, facialHair: false, bald: false, jewelry: false},
+    { number: 6, name: "Olivia", gender: "female", hair: "brown", eyes:"blue", glasses: true, facialHair: false, bald: false, jewelry: false},
+    { number: 7, name: "Phoebe", gender: "female", hair: "blonde", eyes:"blue", glasses: false, facialHair: false, bald: false, jewelry: false},
+    { number: 8, name: "Pierre", gender: "male", hair: "blonde", eyes:"blue", glasses: false, facialHair: true, bald: false, jewelry: false},
+    { number: 9, name: "Tony", gender: "male", hair: "black", eyes:"brown", glasses: true, facialHair: false, bald: false, jewelry: false},
+    { number: 10, name: "Zeke", gender: "male", hair: false, eyes:"green", glasses: false, facialHair: true, bald: true, jewelry: true}
+ 
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 
@@ -24,7 +36,16 @@ const clueMessageEls = document.querySelectorAll(".clue")
 console.log(clueMessageEls)
 const clueCountEl = document.getElementById("clue-count")
 console.log(clueCountEl)
-
+const inputEl = document.getElementById("input")
+console.log(inputEl)
+const submitButtonEl = document.getElementById("submit")
+console.log(submitButtonEl)
+const guessButtonEl = document.getElementById("guess-button")
+console.log(guessButtonEl)
+const instructsButtonEl = document.getElementById("instructions")
+console.log(instructsButtonEl)
+const resetButtonEl = document.getElementById("reset")
+console.log(reset)
 
 
 
