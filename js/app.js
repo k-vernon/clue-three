eafad5eb5a42540efe2e437b97cfcbf3a9b8f9d1
k-vernon <kvernon908@gmail.com>
2022-12-19 12:36:23 -0500

    add dom elements

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 
 /*-------------------------------- Constants --------------------------------*/
-import characters from "./data.js"
+// import characters from "./data.js"
 
 /*---------------------------- Variables (state) ----------------------------*/
 
@@ -11,6 +11,20 @@ let cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 /*------------------------ Cached Element References ------------------------*/
+
+const cardEls = document.querySelectorAll(".card")
+console.log(cardEls)
+const unknownEl = document.getElementById("unknown")
+console.log(unknownEl)
+const messageEl = document.getElementById("game-result")
+console.log(messageEl)
+const subMessageEl = document.getElementById("sub-result")
+console.log(subMessageEl)
+const clueMessageEls = document.querySelectorAll(".clue")
+console.log(clueMessageEls)
+const clueCountEl = document.getElementById("clue-count")
+console.log(clueCountEl)
+
 
 
 
