43c5fccb455ec21f8f6de769e47b3c7e1c5beffa
k-vernon <kvernon908@gmail.com>
2022-12-21 16:02:06 -0500

    change gender key values to 'girl' or 'guy'

--- js/app.js
+++ js/app.js
@@ -2,20 +2,22 @@
 
 /*-------------------------------- Constants --------------------------------*/
 const charactersData = [
-    { number: 1, name: "Amour", gender: "male", hair: "black", eyes:"green", glasses: false, facialHair: false, bald: false, jewelry: true},
+    { number: 1, name: "Amour", gender: "guy", hair: "black", eyes:"green", glasses: false, facialHair: false, bald: false, jewelry: true},
     { number: 2, name: "Ashley", gender: "girl", hair: "black", eyes:"blue", glasses: false, facialHair: false, bald: false, jewelry: true},
-    { number: 3, name: "Bella", gender: "female", hair: "brown", eyes:"brown", glasses: false, facialHair: false, bald: false, jewelry: false},
-    { number: 4, name: "Ember", gender: "female", hair: "red", eyes:"green", glasses: false, facialHair: false, bald: false, jewelry: true},
-    { number: 5, name: "Khalil", gender: "male", hair: "black", eyes:"brown", glasses: false, facialHair: false, bald: false, jewelry: false},
+    { number: 3, name: "Bella", gender: "girl", hair: "brown", eyes:"brown", glasses: false, facialHair: false, bald: false, jewelry: false},
+    { number: 4, name: "Ember", gender: "girl", hair: "red", eyes:"green", glasses: false, facialHair: false, bald: false, jewelry: true},
+    { number: 5, name: "Khalil", gender: "guy", hair: "black", eyes:"brown", glasses: false, facialHair: false, bald: false, jewelry: false},
     { number: 6, name: "Olivia", gender: "girl", hair: "brown", eyes:"blue", glasses: true, facialHair: false, bald: false, jewelry: false},
-    { number: 7, name: "Phoebe", gender: "female", hair: "blonde", eyes:"blue", glasses: false, facialHair: false, bald: false, jewelry: false},
+    { number: 7, name: "Phoebe", gender: "girl", hair: "blonde", eyes:"blue", glasses: false, facialHair: false, bald: false, jewelry: false},
     { number: 8, name: "Pierre", gender: "guy", hair: "blonde", eyes:"blue", glasses: false, facialHair: true, bald: false, jewelry: false},
     { number: 9, name: "Tony", gender: "guy", hair: "black", eyes:"brown", glasses: true, facialHair: false, bald: false, jewelry: false},
-    { number: 10, name: "Zeke", gender: "male", hair: false, eyes:"green", glasses: false, facialHair: true, bald: true, jewelry: true}
+    { number: 10, name: "Zeke", gender: "guy", hair: false, eyes:"green", glasses: false, facialHair: true, bald: true, jewelry: true}
  
 ]
 
-
+const characterKeys = ['name', 'gender', 'hair', 'eyes', 'glasses', 'facialHair', 'bald', 'jewelry'] 
+const characterNames = ['Amour']
+console.log()
 
 /*---------------------------- Variables (state) ----------------------------*/
 
