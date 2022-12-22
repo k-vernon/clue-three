3c4540da0c647e546539fc6bc038e7937d507503
k-vernon <kvernon908@gmail.com>
2022-12-22 18:32:39 -0500

    add placeholders to character cards

--- js/app.js
+++ js/app.js
@@ -2,16 +2,16 @@
 
 /*-------------------------------- Constants --------------------------------*/
 const charactersData = [
-    { number: 1, name: "Amour", gender: "guy", hair: "black", eyes:"green", glasses: false, facialHair: false, bald: false, jewelry: true},
-    { number: 2, name: "Ashley", gender: "girl", hair: "black", eyes:"blue", glasses: false, facialHair: false, bald: false, jewelry: true},
-    { number: 3, name: "Bella", gender: "girl", hair: "brown", eyes:"brown", glasses: false, facialHair: false, bald: false, jewelry: false},
-    { number: 4, name: "Ember", gender: "girl", hair: "red", eyes:"green", glasses: false, facialHair: false, bald: false, jewelry: true},
-    { number: 5, name: "Khalil", gender: "guy", hair: "black", eyes:"brown", glasses: false, facialHair: false, bald: false, jewelry: false},
-    { number: 6, name: "Olivia", gender: "girl", hair: "brown", eyes:"blue", glasses: true, facialHair: false, bald: false, jewelry: false},
-    { number: 7, name: "Phoebe", gender: "girl", hair: "blonde", eyes:"blue", glasses: false, facialHair: false, bald: false, jewelry: false},
-    { number: 8, name: "Pierre", gender: "guy", hair: "blonde", eyes:"blue", glasses: false, facialHair: true, bald: false, jewelry: false},
-    { number: 9, name: "Tony", gender: "guy", hair: "black", eyes:"brown", glasses: true, facialHair: false, bald: false, jewelry: false},
-    { number: 10, name: "Zeke", gender: "guy", hair: false, eyes:"green", glasses: false, facialHair: true, bald: true, jewelry: true}
+    { number: 1, name: "Amour", gender: "guy", hair: "black", eyes:"green", glasses: false, facialHair: false, bald: false, jewelry: true, imgFile: './assets/face.jpeg'},
+    { number: 2, name: "Ashley", gender: "girl", hair: "black", eyes:"blue", glasses: false, facialHair: false, bald: false, jewelry: true, imgFile: './assets/face.jpeg'},
+    { number: 3, name: "Bella", gender: "girl", hair: "brown", eyes:"brown", glasses: false, facialHair: false, bald: false, jewelry: false, imgFile: './assets/face.jpeg'},
+    { number: 4, name: "Ember", gender: "girl", hair: "red", eyes:"green", glasses: false, facialHair: false, bald: false, jewelry: true, imgFile: './assets/face.jpeg'},
+    { number: 5, name: "Khalil", gender: "guy", hair: "black", eyes:"brown", glasses: false, facialHair: false, bald: false, jewelry: true, imgFile: './assets/face.jpeg'},
+    { number: 6, name: "Olivia", gender: "girl", hair: "brown", eyes:"blue", glasses: true, facialHair: false, bald: false, jewelry: false, imgFile: './assets/face.jpeg'},
+    { number: 7, name: "Phoebe", gender: "girl", hair: "blonde", eyes:"blue", glasses: false, facialHair: false, bald: false, jewelry: false, imgFile: './assets/face.jpeg'},
+    { number: 8, name: "Pierre", gender: "guy", hair: "blonde", eyes:"blue", glasses: false, facialHair: true, bald: false, jewelry: false, imgFile: './assets/face.jpeg'},
+    { number: 9, name: "Tony", gender: "guy", hair: "black", eyes:"brown", glasses: true, facialHair: false, bald: false, jewelry: false, imgFile: './assets/face.jpeg'},
+    { number: 10, name: "Zeke", gender: "guy", hair: "red", eyes:"green", glasses: false, facialHair: true, bald: true, jewelry: true, imgFile: './assets/face.jpeg'}
  
 ]
 
@@ -129,7 +129,15 @@ function assignCards(){
     shuffledCharacters.forEach(function(character, charIdx){ 
        cardEls[charIdx].textContent = character.name
         console.log()
-    });
+    })
+    shuffledCharacters.forEach(function(character, charIdx){
+        console.log(cardEls[charIdx]);
+        console.log(character.imgFile)
+        cardEls[charIdx].style.backgroundImage = `url(${character.imgFile})`
+        cardEls[charIdx].style.backgroundSize = "cover"
+        cardEls[charIdx].style.backgroundPosition = "center"
+ 
+    })
 }
 
 function unknownCharacterPick(){
@@ -291,9 +299,11 @@ function checkGuessInput (){
     if (unknownCharacterLowercase === guessInputElLowerCase){
         messageEl.textContent = "YOU WIN" 
         subMessageEl.textContent = "You should be a detective!"
+        winner = true
     } else {
         messageEl.textContent = "YOU LOSE"
         subMessageEl.textContent = "Use your clues more wisely!"
+        winner = false
     }
     guessButtonEl.disabled = true
     guessButtonEl.style.cursor = "not-allowed"
